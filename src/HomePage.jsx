e8952d85ac10878b0f556ee0fb4df0792089b74e
Vander-Graaf <riss-2020@mail.ru>
2024-05-23 10:29:17 +0600

    made basic header

---
+++ src/HomePage.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header.jsx";
+import Content from "./Content.jsx";
+
+function HomePage() {
+  return (
+    <>
+      <Header></Header>
+      <Content></Content>
+    </>
+  );
+}
+
+export default HomePage;
